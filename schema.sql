50585bf3e77dc34cd90f6fce23583e4739ba2962
Drewski1986 <108240844+Drewski1986@users.noreply.github.com>
2022-08-14 15:30:56 -0400

    updated schema for all 6 tables

--- schema.sql
+++ schema.sql
@@ -34,3 +34,22 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no varchar (4) NOT null, 
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title varchar NOT null, 
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+ 
+);
